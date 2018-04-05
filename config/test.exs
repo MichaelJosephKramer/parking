31750171e719f22c9f53ccdede7b0beb374616b2
Michael Joseph Kramer <michael@sage.house>
2018-04-05 14:54:40 -0400

    Initial commit

---
+++ config/test.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :parking, ParkingWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :parking, Parking.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "parking_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
